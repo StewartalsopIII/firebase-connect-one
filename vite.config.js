8ebb25a67b283ed073645ec18c65561c8ac23f17
Stewart Alsop <stewartalsopIII@gmail.com>
2024-08-27 17:11:27 +0000

    added new title and favicon

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-  }
-})
+    host: "0.0.0.0",
+  },
+  base: "./", // This ensures assets are loaded correctly
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom", "react-router-dom"],
+  },
+  publicDir: "public", // Explicitly define the public directory
+});
